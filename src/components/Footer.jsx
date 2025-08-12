3017fc3701112fa765324127cfae99376182aabf
Hitesh04codebot <hiteshkandari@gmail.com>
2025-08-12 21:44:32 +0530

    changes in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <footer className="bg-white text-black text-center py-2 px-2 text-sm sm:text-base">
-      © {new Date().getFullYear()} Your Name. All rights reserved.
+      © {new Date().getFullYear()} Hitesh Kandari. All rights reserved.Made with love.
     </footer>
   )
 }
